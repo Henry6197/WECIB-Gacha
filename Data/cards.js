404dce091c06bd0a28eb16039d9ad265490eb3d0
Treyvon <149333746+TreyvoPrime@users.noreply.github.com>
2026-04-17 12:33:14 -0400

    First storage additions, placeholders
    Added new cards and updated existing ones with additional properties.

--- Data/cards.js
+++ Data/cards.js
@@ -3,6 +3,7 @@ export const cards = [
       id: "vending_machine_24",
       name: "Vending Machine Incident of '24",
       rarity: "super_senior",
+      rarities: "common",
       category: "event",
       pp_gain: 10,
       attack: 95,
@@ -11,14 +12,80 @@ export const cards = [
       image: "images/cards/vending_machine_24.webp"
     },
     {
-        id: "sleeping_rhys",
-        name: "Sleeping Rhys",
+        id: "academic_rigor",
+        name: "Academic Rigor",
         rarity: "super_senior",
-        category: "event",
+        rarities: "uncommon",
+        category: "people",
         pp_gain: 50,
         attack: 110,
         defense: 70,
     
         image: "images/cards/sleeping_rhys.png"
+    },
+    {
+      id: "2_nick",
+      name: "2Nick",
+      rarity: "super_senior",
+      rarities: "common",
+
+      category: "people",
+      pp_gain: 10,
+      attack: 95,
+      defense: 70,
+  
+      image: "images/cards/vending_machine_24.webp"
+    },
+    {
+      id: "ethan_gardner",
+      name: "Ethan Gardner",
+      rarity: "super_senior",
+      rarities: "common",
+
+      category: "people",
+      pp_gain: 10,
+      attack: 95,
+      defense: 70,
+  
+      image: "images/cards/vending_machine_24.webp"
+    },
+    {
+      id: "garder_need_this",
+      name: "Please Mr. Gardner I need this",
+      rarity: "super_senior",
+      rarities: "common",
+
+      category: "people",
+      pp_gain: 10,
+      attack: 95,
+      defense: 70,
+  
+      image: "images/cards/vending_machine_24.webp"
+    },
+    {
+      id: "freshman_jamey",
+      name: "Freshman Jamey",
+      rarity: "freshman",
+      rarities: "common",
+
+      category: "people",
+      pp_gain: 10,
+      attack: 95,
+      defense: 70,
+  
+      image: "images/cards/vending_machine_24.webp"
+    },
+    {
+      id: "camera_man",
+      name: "Cameraman",
+      rarity: "super_senior",
+      rarities: "common",
+
+      category: "people",
+      pp_gain: 10,
+      attack: 95,
+      defense: 70,
+  
+      image: "images/cards/vending_machine_24.webp"
     }
   ];
