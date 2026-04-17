5773e54e01e43c5474a628d4cc00744ca7541fce
Treyvon <149333746+TreyvoPrime@users.noreply.github.com>
2026-04-17 11:50:32 -0400

    Add files via upload

---
+++ Data/cards.js
@@ -0,0 +1,24 @@
+export const cards = [
+    {
+      id: "vending_machine_24",
+      name: "Vending Machine Incident of '24",
+      rarity: "super_senior",
+      category: "event",
+      pp_gain: 10,
+      attack: 95,
+      defense: 70,
+  
+      image: "images/cards/vending_machine_24.webp"
+    },
+    {
+        id: "sleeping_rhys",
+        name: "Sleeping Rhys",
+        rarity: "super_senior",
+        category: "event",
+        pp_gain: 50,
+        attack: 110,
+        defense: 70,
+    
+        image: "images/cards/sleeping_rhys.png"
+    }
+  ];
---
+++ Data/cases.js
@@ -0,0 +1,26 @@
+
+export const cases = [
+  {
+    id: "rtp2_case",
+    name: "RTP-2 Case",
+    costPP: 25,
+    image: "images/cases/crate.jpeg",
+
+    drops: [
+      { cardId: "vending_machine_24"},
+      { cardId: "sleeping_rhys"},
+    ]
+  },
+
+  {
+    id: "rt1_case",
+    name: "RT1 Case",
+    costPP: 40,
+    image: "images/cases/crate.jpeg",
+
+    drops: [
+      { cardId: "rt1_hallway", weight: 30 },
+      { cardId: "vending_machine_24", weight: 3 }
+    ]
+  }
+];
