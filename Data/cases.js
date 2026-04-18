c04b3d20b62d4f9fe1f6bd32730c5ce63f0de3a6
Treyvon <149333746+TreyvoPrime@users.noreply.github.com>
2026-04-17 23:11:22 -0400

    Refactor cases to use Case class constructor

--- Data/cases.js
+++ Data/cases.js
@@ -1,18 +1,28 @@
+export class Case {
+  constructor({ id, name, costPP, image, drops }) {
+    this.id = id;
+    this.name = name;
+    this.costPP = costPP;
+    this.image = image;
+    this.drops = drops;
+  }
+}
+
 
 export const cases = [
-  {
+  new Case({
     id: "rtp2_case",
     name: "RTP-2 Case",
     costPP: 25,
     image: "images/cases/crate.jpeg",
 
     drops: [
-      { cardId: "vending_machine_24"},
-      { cardId: "sleeping_rhys"},
+      { cardId: "vending_machine_24" },
+      { cardId: "academic_rigor" }
     ]
-  },
+  }),
 
-  {
+  new Case({
     id: "rt1_case",
     name: "RT1 Case",
     costPP: 40,
@@ -22,5 +32,5 @@ export const cases = [
       { cardId: "rt1_hallway" },
       { cardId: "vending_machine_24" }
     ]
-  }
+  })
 ];
