1b4d1039d753b3d1ac38f18af8227d976bd6dae8
Treyvon <149333746+TreyvoPrime@users.noreply.github.com>
2026-04-17 23:11:05 -0400

    Change cards to use Card class instances
    Refactored card data structure to use Card class.

--- Data/cards.js
+++ Data/cards.js
@@ -1,91 +1,62 @@
+//placeholder values
+export class Card {
+  constructor({
+    id,
+    name,
+    rarity,
+    tier,
+    category,
+    pp_gain,
+    attack,
+    defense,
+    image
+  }) {
+    this.id = id;
+    this.name = name;
+    this.rarity = rarity;
+    this.tier = tier;
+    this.category = category;
+    this.pp_gain = pp_gain;
+    this.attack = attack;
+    this.defense = defense;
+    this.image = image;
+  }
+}
+
 export const cards = [
-    {
-      id: "vending_machine_24",
-      name: "Vending Machine Incident of '24",
-      rarity: "super_senior",
-      rarities: "common",
-      category: "event",
-      pp_gain: 10,
-      attack: 95,
-      defense: 70,
-  
-      image: "images/cards/vending_machine_24.webp"
-    },
-    {
-        id: "academic_rigor",
-        name: "Academic Rigor",
-        rarity: "super_senior",
-        rarities: "uncommon",
-        category: "people",
-        pp_gain: 50,
-        attack: 110,
-        defense: 70,
-    
-        image: "images/cards/sleeping_rhys.png"
-    },
-    {
-      id: "2_nick",
-      name: "2Nick",
-      rarity: "super_senior",
-      rarities: "common",
+  new Card({
+    id: "vending_machine_24",
+    name: "Vending Machine Incident of '24",
+    rarity: "super_senior",
+    tier: "common",
+    category: "event",
+    pp_gain: 10,
+    attack: 95,
+    defense: 70,
+    image: "images/cards/vending_machine_24.webp"
+  }),
 
-      category: "people",
-      pp_gain: 10,
-      attack: 95,
-      defense: 70,
-  
-      image: "images/cards/vending_machine_24.webp"
-    },
-    {
-      id: "ethan_gardner",
-      name: "Ethan Gardner",
-      rarity: "super_senior",
-      rarities: "common",
+  new Card({
+    id: "academic_rigor",
+    name: "Academic Rigor",
+    rarity: "super_senior",
+    tier: "uncommon",
+    category: "people",
+    pp_gain: 50,
+    attack: 110,
+    defense: 70,
+    image: "images/cards/sleeping_rhys.png"
+  }),
 
-      category: "people",
-      pp_gain: 10,
-      attack: 95,
-      defense: 70,
-  
-      image: "images/cards/vending_machine_24.webp"
-    },
-    {
-      id: "garder_need_this",
-      name: "Please Mr. Gardner I need this",
-      rarity: "super_senior",
-      rarities: "common",
-
-      category: "people",
-      pp_gain: 10,
-      attack: 95,
-      defense: 70,
-  
-      image: "images/cards/vending_machine_24.webp"
-    },
-    {
-      id: "freshman_jamey",
-      name: "Freshman Jamey",
-      rarity: "freshman",
-      rarities: "common",
-
-      category: "people",
-      pp_gain: 10,
-      attack: 95,
-      defense: 70,
-  
-      image: "images/cards/vending_machine_24.webp"
-    },
-    {
-      id: "camera_man",
-      name: "Cameraman",
-      rarity: "super_senior",
-      rarities: "common",
-
-      category: "people",
-      pp_gain: 10,
-      attack: 95,
-      defense: 70,
-  
-      image: "images/cards/vending_machine_24.webp"
-    }
-  ];
+  new Card({
+    id: "2_nick",
+    name: "2Nick",
+    rarity: "super_senior",
+    tier: "common",
+    category: "people",
+    pp_gain: 10,
+    attack: 95,
+    defense: 70,
+    image: "images/cards/vending_machine_24.webp"
+  })
+];
