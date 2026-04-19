a3399d04f446c66f224abc281a8b09b44f6cf995
Treyvon <149333746+TreyvoPrime@users.noreply.github.com>
2026-04-19 10:10:50 -0400

    Refactor cases and add RARITIES and CASES constants

--- Data/cases.js
+++ Data/cases.js
@@ -1,36 +1,31 @@
-export class Case {
-  constructor({ id, name, costPP, image, drops }) {
-    this.id = id;
-    this.name = name;
-    this.costPP = costPP;
-    this.image = image;
-    this.drops = drops;
-  }
-}
+export const RARITIES = {
+  common: { label: "Freshman", color: "#dee4ff", weight: 65 },
+  uncommon: { label: "Sophomore", color: "#8fff74", weight: 25 },
+  rare: { label: "Junior", color: "#4781ff", weight: 8 },
+  epic: { label: "Senior", color: "#922ce6", weight: 1.8 },
+  secret: { label: "★ Super Senior ★", color: "#ffd700", weight: 0.2 },
+};
 
-
-export const cases = [
-  new Case({
-    id: "rtp2_case",
-    name: "RTP-2 Case",
-    costPP: 25,
+export const CASES = [
+  {
+    id: "starter_case",
+    name: "WECIB Starter Case",
+    pp_cost: 1.0,
     image: "images/cases/crate.jpeg",
-
-    drops: [
+    items: [
       { cardId: "vending_machine_24" },
-      { cardId: "academic_rigor" }
+      { cardId: "academic_rigor" },
+      { cardId: "2_nick" }
     ]
-  }),
-
-  new Case({
+  },
+  {
     id: "rt1_case",
     name: "RT1 Case",
-    costPP: 40,
+    pp_cost: 5.0,
     image: "images/cases/crate.jpeg",
-
-    drops: [
-      { cardId: "rt1_hallway" },
+    items: [
+      { cardId: "2_nick" },
       { cardId: "vending_machine_24" }
     ]
-  })
+  }
 ];
